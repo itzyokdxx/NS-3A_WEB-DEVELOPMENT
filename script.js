15187da8c7336525ce94b9f43475483b231fa86c
itzyokdxx <131840627+itzyokdxx@users.noreply.github.com>
2023-10-11 21:03:07 +0800

    Added animations.

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Aspiring Programmer", "Aspiring UX/UI Designer", 
+    "Aspiring Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+})
