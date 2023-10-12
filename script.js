81653953abd3fdc94501c1b83eea6b80b6e37f47
itzyokdxx <131840627+itzyokdxx@users.noreply.github.com>
2023-10-12 10:36:51 +0800

    c

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Aspiring Programmer", "Aspiring UX/UI Designer", 
+    strings: ["Aspiring Graphic Artist", "Aspiring UX/UI Designer", 
     "Aspiring Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
