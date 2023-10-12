596d49cb69c06f333c662aafc718b4c22a8fd0b8
itzyokdxx <131840627+itzyokdxx@users.noreply.github.com>
2023-10-12 17:39:03 +0800

    Updated.

--- script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Aspiring Graphic Artist", "Aspiring UX/UI Designer", 
-    "Aspiring Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 500,
-    loop: true
-})
